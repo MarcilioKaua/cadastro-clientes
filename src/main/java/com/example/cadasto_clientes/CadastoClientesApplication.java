d8de45b75241fae8dd64f19a8a0d379b81176dd9
Marcilio Silva <marciliokaua12@gmail.com>
2024-12-28 12:51:18 -0300

    iniciando projeto

---
+++ src/main/java/com/example/cadasto_clientes/CadastoClientesApplication.java
@@ -0,0 +1,13 @@
+package com.example.cadasto_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastoClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastoClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cadasto_clientes/CadastoClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadasto_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastoClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
