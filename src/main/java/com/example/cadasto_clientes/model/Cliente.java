d705d8375340a4a7bfb5e208ee2527a39a55c2c7
Marcilio Silva <marciliokaua12@gmail.com>
2024-12-28 13:31:48 -0300

    adicionando entidade e banco de dados em memoria

---
+++ src/main/java/com/example/cadasto_clientes/model/Cliente.java
@@ -0,0 +1,33 @@
+package com.example.cadasto_clientes.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String telefone;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false)
+    private String estado;
+}
